2e1574f10cdb109b78313b706f7fa376c2abe913
Ronak-0052 <ronak0052.sharma@gmail.com>
2025-08-15 18:32:55 +0530

    Verification of a number is natural number or not

--- Hello.c
+++ Hello.c
@@ -1,22 +1,14 @@
-/*WAP to to check the given number is armstrong or not .*/
+/*WAP to to check the given number is natural number or not (natural number start from 1) .*/
 #include<stdio.h>
 int main() {
-    int n,r,temp,sum=0,cube;
+    int n;
     printf("Enter a number : ");
     scanf("%d",&n);
-    temp = n;
-    for(int i = 0; n != 0; i++){
-        r = n%10;
-        cube = r*r*r;
-        sum = sum+cube;
-        n = n/10;
-    }
-
-    if(sum==temp) {
-        printf("\n%d is an armstrong number",temp);
+    if(n>=1) {
+        printf("\n%d is a natural number",n);
     }
     else {
-        printf("\n%d is not an armstrong number",temp);
+        printf("\n%d is not a natural number",n);
     }
     return 0;
 }
