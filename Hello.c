929bd240c828c219cc7bb0a523715388f9ee7265
Ronak-0052 <ronak0052.sharma@gmail.com>
2025-08-16 17:11:30 +0530

    Verification of a number is prime number or not

--- Hello.c
+++ Hello.c
@@ -1,12 +1,30 @@
-/*WAP to print a 4*5 matrix of star element(*) with the help of Nested loop  .*/
+/*WAP to print to check if a number is prime or not.*/
 #include<stdio.h>
 int main() {
-    int i,j;
-    for(i=0; i<4; i++) {
-    for(j=0; j<5; j++) {
-        printf("* ");
+    int n, i,prime=1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n <= 1) {
+        prime=0;
     }
-    printf("\n");
+    else{
+        for (i=2; i <= n/2; i++) {
+            if (n%i ==0)
+            {
+                prime = 0;
+                break;
+            }
+        }
     }
+
+    if (prime)
+    {
+        printf("The %d is a prime number", n);
+    }
+    else
+    {
+        printf("The %d is not a prime number", n);
+    }
+
     return 0;
 }
